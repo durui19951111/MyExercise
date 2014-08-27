dc6ca5a94f50f7bdd7bf950a81c56832d2152edc
durui19951111 <durui19951111@qq.com>
2014-08-27 15:18:35 +0800

    .upedate code

---
+++ java/TestJava2_1.java
@@ -0,0 +1,12 @@
+   // TestJava2_1.java，java 的简单范例 
+   public class TestJava2_1 
+   { 
+       public static void main(String args[]) 
+       { 
+           int num ;    //  声明一个整型变量 num 
+           num = 3 ;    //  将整型变量赋值为 3 
+           //  输出字符串，这里用"+"  号连接变量 
+           System.out.println("这是数字  "+num); 
+           System.out.println("我有  "+num+"  本书！"); 
+       } 
+    } 
